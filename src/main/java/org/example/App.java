764d113e2d68d42989666fe2dd33518a1d0db9f4
Caitlin-Fabian <89735646+Caitlin-Fabian@users.noreply.github.com>
2021-09-13 21:49:47 -0400

    Initial commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("How many people? ");
+        int people = input.nextInt();
+        System.out.print("How many pizzas do you have? ");
+        int pizzas = input.nextInt();
+        int slices = people * pizzas;
+        System.out.println(people + " people with " + pizzas + " pizzas ("+ slices + " slices)");
+        int each = slices / people;
+
+        System.out.println("Each person gets " + each + " pieces of pizzas.");
+        int leftOver = slices % people;
+        System.out.println("There are " + leftOver + "left over pieces.");
+    }
+}
